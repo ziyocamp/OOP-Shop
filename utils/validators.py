5addd07c8b1d9dafc7b1430b3890ac0db3bff244
Keldibek2306 <jalilovmadaminbek2@gmail.com>
2025-08-06 12:07:40 +0500

    name validtors

---
+++ utils/validators.py
@@ -0,0 +1,9 @@
+def validate_name(text: str) -> bool:
+    if not text:
+        print(text,"name bosh bolmasligi bolishi kerak")
+    elif not text.isalpha():
+        print(text,"Katta xarfdan iborat bolishi kerak")
+    return True
+    name = text
+    name != text
+    return True
