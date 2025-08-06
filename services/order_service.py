7707821075e957d1a1eeb4ff2f606242ca3b8edf
VOHID2308 <xtovuqov@gmail.com>
2025-08-06 16:11:58 +0500

    order_service crated

--- services/order_service.py
+++ services/order_service.py
@@ -1,14 +1,43 @@
+import json
 from models.order import Order
 from models.product import Product
 from models.user import User
 
 class OrderService:
     def __init__(self):
-        self.orders = []
+        self.file_path = "data/db.json"
+        self.orders = self.load_orders()
+
+    def load_orders(self):
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as file:
+                data = json.load(file)
+                orders = []
+                for item in data.get("orders", []):
+                    user = User(name=item["user"]["name"])
+                    products = [Product(p["name"], p["price"]) for p in item["products"]]
+                    orders.append(Order(user, products))
+                return orders
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
+
+    def save_orders(self):
+        data = {
+            "orders": [
+                {
+                    "user": {"name": order.user.name},
+                    "products": [{"name": p.name, "price": p.price} for p in order.products]
+                }
+                for order in self.orders
+            ]
+        }
+        with open(self.file_path, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
 
     def create_order(self, user: User, products: list[Product]) -> None:
         order = Order(user=user, products=products)
         self.orders.append(order)
+        self.save_orders()
         print("Buyurtma muvaffaqiyatli yaratildi.")
 
     def get_orders(self) -> list:
